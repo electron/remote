f4ef1ce63185ec0acb46b5f3bb3229c87df49728
Jeremy Apthorp <nornagon@nornagon.net>
2020-02-19 18:20:18 -0800

    test: override default window-all-closed handler

--- test/index.js
+++ test/index.js
@@ -1,4 +1,10 @@
 const chai = require('chai')
 chai.use(require('chai-as-promised'))
 chai.use(require('dirty-chai'))
-require('mocha/bin/mocha')
+const { app } = require('electron')
+
+app.on('window-all-closed', () => null)
+
+app.whenReady().then(() => {
+  require('mocha/bin/mocha')
+})
