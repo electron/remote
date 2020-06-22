5ff9d452f551ec31e09538ccf0fed09bd0914e71
Jeremy Rose <nornagon@nornagon.net>
2020-06-22 10:04:18 -0700

    chore: release 1.0.0

--- index.js
+++
@@ -1,1 +0,0 @@
-throw new Error(`@electron/remote cannot be required directly. Instead require("@electron/remote/${process.type === 'browser' ? 'main' : 'renderer'}").`)
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,1 +1,3 @@
+if (process.type === 'browser')
+  throw new Error(`"@electron/remote" cannot be required in the browser process. Instead require("@electron/remote/main").`)
 export * from './remote'
