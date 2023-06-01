10e02d5711620032d8c7df6554a1e6b3f2e6d293
Juan Irache <yrache@gmail.com>
2023-06-02 00:15:52 +0200

    Checking isDesktopCapturerEnabled is no longer necessary or possible. Fixes #153
    Context https://github.com/electron/electron/pull/38412

--- src/common/module-names.ts
+++ src/common/module-names.ts
@@ -15,6 +15,7 @@ export const browserModuleNames = [
   'contentTracing',
   'crashReporter',
   'dialog',
+  'desktopCapturer',
   'globalShortcut',
   'ipcMain',
   'inAppPurchase',
@@ -45,10 +46,6 @@ export const browserModuleNames = [
 ].concat(commonModuleNames);
 
 const features = getElectronBinding('features');
-
-/* if (!features || features.isDesktopCapturerEnabled()) {
-  browserModuleNames.push('desktopCapturer');
-} */
 
 if (!features || features.isViewApiEnabled()) {
   browserModuleNames.push('ImageView');
